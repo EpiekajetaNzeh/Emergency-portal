98e98f7165fd5240279336e2d9d9e466635824a1
EpiekajetaNzeh <163450496+EpiekajetaNzeh@users.noreply.github.com>
2025-07-07 23:20:43 +0100

    Emergency-Portal

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+			<div class="wthree-copyright">
+			  <p> Emergency Ambulance Portal System Copyright By <a href="https://epie-portfolio-responsive.netlify.app" target="_blank" >KAJETA EPIE</a></p>
+			</div>
+		  </div>
